83debb240ed024f6068fa7a612d9075ef5904df6
Yusheng Zhao <yushengzhao2020@outlook.com>
2024-04-15 14:12:53 +0800

    up

--- examples/rain_shark.jl
+++ examples/rain_shark.jl
@@ -6,17 +6,39 @@ using OptQit, Yao, MosekTools, Convex
 # The first step is to declare variables 
 # The order of the arguments is the size, the sign, and then the Convex.VarType (i.e., integer, binary, or continuous), and any may be omitted to use the default.
 size = (2,1)
-sign = Positive() # or Negative()
-vartype = IntVar # or BinVar
+sign = Positive() # or Negative(), NoSign(), ComplexSign()
+vartype = IntVar # or BinVar, ContVar
+
 @show y = Variable(size,sign,vartype)
 
-z = Semidefinite(2)
+z = Semidefinite(2) # alias for  Variable((m, m), NoSign(), ContVar)
+z = ComplexVariable(size) # alias for Variable(size,ComplexSign(),ContVar)
 
-cx1 = ComplexVariable()
+# %% [markdown]
+# Ofcourse, it is easy to define your own variables. For example, let's define a probability amplitude variable
+
+# %%
+mutable struct ProbabilityAmpVector <: Convex.AbstractVariable
+    head::Symbol
+    id_hash::UInt64
+    size::Tuple{Int, Int}
+    value::Convex.ValueOrNothing
+    vexity::Convex.Vexity
+    function ProbabilityAmpVector(d)
+        this = new(:ProbabilityAmpVector, 0, (d,1), nothing, Convex.AffineVexity())
+        this.id_hash = objectid(this)
+        this
+    end
+end
+
+Convex.constraints(p::ProbabilityAmpVector) = [ norm(p,2) == 1 ]
+Convex.sign(::ProbabilityAmpVector) = Convex.ComplexSign()
+Convex.vartype(::ProbabilityAmpVector) = Convex.ContVar
+
+(p::ProbabilityVector)(x) = dot(p, x)
 
 x1 = Variable(1)
 x2 = Variable()
-
 
 
 # %% [markdown]
